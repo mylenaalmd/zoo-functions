652d56e0c35d344254b20756c93b13c173d57f34
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-04-25 13:59:37 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,34 +1,48 @@
 // const en = require('faker/lib/locales/en');
-// const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// function countEntrants(entrants) {
-//   // seu código aqui
-//   const dados = entrants.map(({ e }) => e.name);
-//   console.log(dados);
-//   // const obj = {};
-//   // pop.reduce((arr, curr) => {
-//   //   if (age < 18) {
-//   //     obj[child] = +1;
-//   //   } if (age >= 18) {
-//   //     obj[adult] = +1;
-//   //   } if (age >= 50) {
-//   //     obj[senior] = +1;
-//   //   }
-//   // });
-//   // return obj;
-// }
+const { prices } = data;
 
-// function calculateEntry(entrants) {
-//   // seu código aqui
-// }
-// const entrants = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
-// console.log(calculateEntry(entrants));
+const entrantsTest = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
-// module.exports = { calculateEntry, countEntrants };
+function countEntrants(entrants) {
+  const dados = entrants.map((e) => e.age);
+  const obj = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  dados.forEach((idade) => {
+    if (idade < 18) {
+      obj.child += 1;
+    } if (idade >= 18 && idade < 50) {
+      obj.adult += 1;
+    } if (idade >= 50) {
+      obj.senior += 1;
+    }
+  });
+  return obj;
+}
+function calculateEntry(entrants) {
+  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  if (!Object.keys(entrants).length) {
+    return 0;
+  }
+  const obj = countEntrants(entrants);
+  const preço = obj.adult * prices.adult + obj.child * prices.child + obj.senior * prices.senior;
+  return preço;
+}
+
+console.log(calculateEntry(entrantsTest));
+
+module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,12 +1,63 @@
-// const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// function getAnimalMap(options) {
-//   // seu código aqui
-//   const categLoc = {};
-//   if (!options) {
-//     const l = data.species.reduce((especies) => data.species.filter((loc) => especies.location === loc.location));
-//     console.log(l);
-//   }
-// }
+const { species } = data;
 
-// module.exports = getAnimalMap;
+const localizacao = species.map((especies) => especies.location);
+const animalsLocation = () => {
+  const categLoc = {};
+  localizacao.forEach((l) => {
+    categLoc[l] = species.filter((especie) => especie.location === l)
+      .map((s) => s.name);
+  });
+  return categLoc;
+};
+function animalsSex(options) {
+  let verificity = [];
+  const local = animalsLocation();
+  Object.entries(local).forEach((v) => {
+    v[1].forEach((item) => {
+      const entrada = { [item]: species.find((especie) => especie.name === item)
+        .residents.filter((sex) => sex.sex === options.sex)
+        .map((nome) => nome.name) };
+      if (options.sorted === true) {
+        entrada[item].sort();
+      }
+      verificity.push(entrada);
+    });
+    local[v[0]] = verificity;
+    verificity = [];
+  });
+  return local;
+}
+function animalsName(options) {
+  let verificity = [];
+  const local = animalsLocation();
+  Object.entries(local).forEach((v) => {
+    v[1].forEach((item) => {
+      const entrada = { [item]: species.find((especie) =>
+        especie.name === item).residents.map((nome) => nome.name) };
+      if (options.sorted === true) {
+        entrada[item].sort();
+      }
+      verificity.push(entrada);
+    });
+    local[v[0]] = verificity;
+    verificity = [];
+  });
+  return local;
+}
+animalsSex({ sex: 'male' });
+function getAnimalMap(options) {
+  // seu código aqui
+  if (!options || !options.includeNames) {
+    return animalsLocation();
+  }
+  if (options.sex === 'male' || options.sex === 'female') {
+    return animalsSex();
+  }
+  return animalsName(options);
+}
+console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
+
+module.exports = getAnimalMap;
+// Ajuda da colega Ster pra resolver esse requisito;
