ed7d6e1b313b933f3488eb2d49a0493bf4fc0f90
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-04-25 09:02:44 -0300

    atualizando erros do lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,34 +1,34 @@
-const en = require('faker/lib/locales/en');
-const data = require('../data/zoo_data');
+// const en = require('faker/lib/locales/en');
+// const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-  const dados = entrants.map(({ e }) => e.name);
-  console.log(dados);
-  // const obj = {};
-  // pop.reduce((arr, curr) => {
-  //   if (age < 18) {
-  //     obj[child] = +1;
-  //   } if (age >= 18) {
-  //     obj[adult] = +1;
-  //   } if (age >= 50) {
-  //     obj[senior] = +1;
-  //   }
-  // });
-  // return obj;
-}
+// function countEntrants(entrants) {
+//   // seu código aqui
+//   const dados = entrants.map(({ e }) => e.name);
+//   console.log(dados);
+//   // const obj = {};
+//   // pop.reduce((arr, curr) => {
+//   //   if (age < 18) {
+//   //     obj[child] = +1;
+//   //   } if (age >= 18) {
+//   //     obj[adult] = +1;
+//   //   } if (age >= 50) {
+//   //     obj[senior] = +1;
+//   //   }
+//   // });
+//   // return obj;
+// }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-console.log(calculateEntry(entrants));
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+// console.log(calculateEntry(entrants));
 
-module.exports = { calculateEntry, countEntrants };
+// module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,12 +1,12 @@
-const data = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-  const categLoc = {};
-  if (!options) {
-    const l = data.species.map((especies) => data.species.filter((loc) => especies.location === loc.location));
-    console.log(l);
-  }
-}
+// function getAnimalMap(options) {
+//   // seu código aqui
+//   const categLoc = {};
+//   if (!options) {
+//     const l = data.species.reduce((especies) => data.species.filter((loc) => especies.location === loc.location));
+//     console.log(l);
+//   }
+// }
 
-module.exports = getAnimalMap;
+// module.exports = getAnimalMap;
