35e1236b0c0d4dbced4133ad2c7a42b0f57298c9
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-05-08 23:04:53 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalsResponsible = data.employees.find((item) => item.id === id).responsibleFor[0];
+  const animalsId = data.species.find((item) => item.id === animalsResponsible).residents;
+  const animalAge = animalsId.sort((a, b) => b.age - a.age)[0];
+  return Object.values(animalAge);
 }
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,56 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const days = Object.keys(data.hours);
+
+const animalsTimesOpen = () => {
+  // const animalsDay = {};
+  // const j = Object.entries(data.hours);
+  // // j.forEach((item) => console.log(Object.entries(item[1])));
+  // // console.log(j);
+  // Object.entries(data.hours).forEach((horario) => {
+  //   animalsDay[horario[0]] = {};
+
+  //   // const numbs = j.concat();
+  //   // j.forEach((o) => o.concat());
+  //   // console.log(o);
+  // //   const timeZoo = { 'officeHour' :  `Open from ${[0]}am until ${[0]}pm `,
+  // //   'exhibition': [],
+  // // animalsDay.push(timeZoo);
+  // });
+  // console.log(animalsDay);
+};
+function getAnimalByDay(day) {
+  return data.species.filter((animal) => animal.availability.find((dia) => dia === day).map((bicho) => bicho.name));
 }
+const animalsDay = (scheduleTarget) => {
+  const animalsAvailable = {};
+  const time = Object.entries(data.hours).find((item) => item[0] === scheduleTarget);
+  console.log(time);
+  const horario = Object.values(time[1]);
+  const array = getAnimalByDay(scheduleTarget);
+  console.log(array);
+  animalsAvailable[scheduleTarget] = {
+    officeHour: `Open from ${horario[0]}am until ${horario[1]}pm`,
+    exhibition: 'animalsExibition' };
+  return animalsAvailable;
+  //     'exhibition': [ 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes' ],
+};
+
+const animalsName = (scheduleTarget) => {
+  const daysOpen = data.species.find((item) => item.name === scheduleTarget)
+    .availability.map((l) => l);
+  return daysOpen;
+};
+
+function getSchedule(scheduleTarget) {
+  // if (!scheduleTarget || scheduleTarget !== animals || scheduleTarget !== days) {
+  //   return animalsTimesOpen();
+  // }
+  if ((days.some((item) => item === scheduleTarget)) === true) {
+    return animalsDay(scheduleTarget);
+  }
+  return animalsName(scheduleTarget);
+}
+console.log(getSchedule('Wednesday'));
 
 module.exports = getSchedule;
